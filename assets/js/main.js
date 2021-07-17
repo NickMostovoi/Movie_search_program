a963a9363e7da560cb5ffbfee43078072b41cd9e
Mykola Mostovoi <nick211996@gmail.com>
2021-07-17 14:29:55 +0300

    Добавлена разметка, работа над стилем

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,31 +1,60 @@
 let movieList = null;
 
+const createStyle = () => {
+  const headStyle = document.createElement('style');
+
+  headStyle.innerHTML = `
+      *           { box-sizing: border-box; }
+      body        { margin: 0; }
+      .wrapper    { padding: 20px; }
+      .movies {
+          display: grid;
+          grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+          gap: 20px;
+      }
+      
+      .movie {
+          display: flex;
+          align-content: center;
+          justify-content: center;
+      }
+      
+      .movie__image {
+          width: 100%;
+          object-fit: cover
+      }
+    `;
+
+  document.head.appendChild(headStyle);
+};
+
 const createMarkup = () => {
-    const movieWrapper = document.createElement('div');
-    const movies = document.createElement('div');
+  const movieWrapper = document.createElement('div');
+  const movies = document.createElement('div');
 
-    movieWrapper.classList.add('wrapper');
+  movieWrapper.classList.add('wrapper');
 
-    movies.classList.add('movies');
-    movieWrapper.appendChild(movies);
+  movies.classList.add('movies');
+  movieWrapper.appendChild(movies);
 
-    document.body.appendChild(movieWrapper);
+  document.body.appendChild(movieWrapper);
 
-    movieList = document.querySelector('.movies');
+  movieList = document.querySelector('.movies');
 };
 
 const addMovieToList = (movie) => {
-    const item = document.createElement('div');
-    const img = document.createElement('img');
-    
-    img.src = movie.Poster;
-    img.classList.add('movie__image');
+  const item = document.createElement('div');
+  const img = document.createElement('img');
 
-    item.classList.add('movie');
+  img.src = movie.Poster;
+  img.classList.add('movie__image');
 
-    item.appendChild(img);
+  item.classList.add('movie');
 
-    movieList.appendChild(item);
+  item.appendChild(img);
+
+  movieList.appendChild(item);
 };
 
 createMarkup();
+createStyle();
--- assets/js/xhr.js
+++ assets/js/xhr.js
@@ -1,16 +1,19 @@
+
+const siteUrl = 'http://www.omdbapi.com/';
+
 const getData = (url) => fetch(url).then((res) => res.json()).then((json) => json.Search);
 
 const search = 'Iron man';
-const search2 = 'Superman';
-const search3 = 'Batman';
+// const search2 = 'Superman';
+// const search3 = 'Batman';
 
-const ironman = getData(`http://www.omdbapi.com/?apikey=379c8492&s=${search}`)
-const superman = getData(`http://www.omdbapi.com/?apikey=379c8492&s=${search2}`)
-const batman = getData(`http://www.omdbapi.com/?apikey=379c8492&s=${search3}`)
+// const ironman = getData(`http://www.omdbapi.com/?apikey=379c8492&s=${search}`)
+// const superman = getData(`http://www.omdbapi.com/?apikey=379c8492&s=${search2}`)
+// const batman = getData(`http://www.omdbapi.com/?apikey=379c8492&s=${search3}`)
 
-batman.then((movies) => movies.forEach((movie) => addMovieToList(movie)));
+// batman.then((movies) => movies.forEach((movie) => addMovieToList(movie)));
 
 
-// getData(`http://www.omdbapi.com/?apikey=379c8492&s=${search}`)
-//     .then((movies) => movies.forEach((movie) => console.log(movie)));
+getData(`${siteUrl}?apikey=379c8492&s=${search}`)
+    .then((movies) => movies.forEach((movie) => addMovieToList(movie)));
 
