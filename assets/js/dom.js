68d8cc71244afb810526665bd3090ac7fd4f7c37
Mykola Mostovoi <nick211996@gmail.com>
2021-07-27 14:30:56 +0300

    Правки

--- assets/js/dom.js
+++ assets/js/dom.js
@@ -6,82 +6,8 @@ export const createStyle = () => {
   const headStyle = document.createElement('style');
 
   headStyle.innerHTML = `    
-    * {
-    box-sizing: border-box;
-    }
-    body {
-      margin: 0;
-      font-family: "Comfortaa";
-      background: linear-gradient(
-        135deg,
-        rgb(149, 149, 149) 0%,
-        rgb(13, 13, 13) 46%,
-        rgb(1, 1, 1) 50%,
-        rgb(10, 10, 10) 53%,
-        rgb(78, 78, 78) 76%,
-        rgb(56, 56, 56) 87%,
-        rgb(27, 27, 27) 100%
-      );
-      background-size: 100% 100vh;
-      background-attachment: fixed;
-    }
-    .container {
-      padding: 20px;
-      max-width: 1280px;
-      margin: auto;
-    }
-    h1 {
-      font-size: 33px;
-      color: #0bb4c0;
-      text-align: center;
-    }
-    .movies {
-      display: grid;
-      grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
-      gap: 20px;
-    }
-    .movie {
-      display: flex;
-      align-content: center;
-      justify-content: center;
-    }
-    .movie__image {
-      width: 100%;
-      object-fit: cover;
-    }
-    .search {
-      margin-bottom: 30px;
-    }
-    .search__input {
-      padding: 10px 15px;
-      width: 60%;
-      display: block;
-      border: 2px solid #0bb4c0;
-      border-radius: 5px;
-      margin-bottom: 10px;
-      margin-left: auto;
-      margin-right: auto;
-    }
-    .search__label-input {
-      display: block;
-      margin-bottom: 7px;
-    }
-    .search__checkbox {
-      margin-left: 26%;
-    }
-    .search__label-checkbox {
-      font-size: 12px;
-      display: block;
-      margin-top: -17px;
-      margin-left: 28%;
-      color: #ffffff;
-      text-shadow: 0 0 0.8em #ffffff;
-    }
-    @media (max-width: 786px) {
-      .search__checkbox {
-        margin-left: 24%;
-      }
-    }
+    
+    
   `;
 
   document.head.appendChild(headStyle);
